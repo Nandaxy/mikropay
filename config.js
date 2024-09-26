f437e4470834296ec4dcc98a5db993e8012ab47f
Nandaxy <indri.khaulia12@gmail.com>
2024-09-26 07:42:43 +0700

    fix error

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
     mongo_uri: "",
     userApp: {
@@ -6,12 +5,3 @@ module.exports = {
         password: process.env.APP_PASSWORD || "admin"
     }
 };
-=======
-module.exports = {
-    mongo_uri: "",
-    userApp: {
-        username: process.env.APP_USERNAME || "admin",
-        password: process.env.APP_PASSWORD || "admin"
-    }
-};
->>>>>>> a3bee85c5870023818779dc67203111c159037e7
