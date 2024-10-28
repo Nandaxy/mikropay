8ac32a911d7aa4bdcb1e5a54d0625f814f31f693
Nanda <138947445+Nandaxy@users.noreply.github.com>
2024-10-28 21:30:20 +0700

    fix deploy.sh on linux

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Menginstall PM2..."
 if command -v pm2 &>/dev/null; then
     echo "PM2 sudah terinstall."
@@ -7,29 +9,23 @@ else
 fi
 
 CONFIG_FILE="fe/src/lib/config.js"
-if [ -f "$CONFIG_FILE" ]; then
-    sed -i 's|export const BASE_URL = "http://localhost:5000/";|export const BASE_URL = "/";|' "$CONFIG_FILE" || 
-    { echo "Gagal mengubah file config.js"; exit 1; }
-else
-    echo "File config.js tidak ditemukan."
-    exit 1
-fi
+
+sed -i 's|export const BASE_URL = "http://localhost:5000/";|export const BASE_URL = "/";|' "$CONFIG_FILE" || 
+{ echo "Gagal mengubah file config.js"; exit 1; }
 
 echo "Menghapus isi folder be/views..."
-rm -rf be/views/
+rm -rf be/views/*
+mkdir -p be/views 
 
 echo "Menginstall module React JS..."
-(
-    cd fe || exit
-    npm install &&
-    npm run build &&
-    mv dist/ ../be/views || { echo "Gagal memindahkan build ke folder views"; exit 1; }
-)
+cd fe || exit
+npm install &&
+npm run build &&
+mv dist/* ../be/views || { echo "Gagal memindahkan build ke folder views"; exit 1; }
 
-if [ -f "$CONFIG_FILE" ]; then
-    sed -i 's|export const BASE_URL = "/";|export const BASE_URL = "http://localhost:5000/";|' "$CONFIG_FILE" || 
-    { echo "Gagal mengembalikan file config.js ke pengaturan semula"; exit 1; }
-fi
+cd ..
+sed -i 's|export const BASE_URL = "/";|export const BASE_URL = "http://localhost:5000/";|' "$CONFIG_FILE" || 
+{ echo "Gagal mengembalikan file config.js ke pengaturan semula"; exit 1; }
 
 echo "Menginstall module Express..."
 cd be || exit
@@ -41,9 +37,4 @@ pm2 start server.js || { echo "Gagal memulai server dengan PM2"; exit 1; }
 
 echo "Mendapatkan IP Address..."
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
-if [ -z "$IP_ADDRESS" ]; then
-    echo "Gagal mendapatkan IP Address, Tetapi Deployment berhasil dan berjalan pada port 5000"
-    exit 1
-else
-    echo "Deployment berhasil. Buka browser di http://$IP_ADDRESS:5000"
-fi
+echo "Deployment berhasil. Buka browser di http://$IP_ADDRESS:5000"
